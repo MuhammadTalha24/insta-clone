33554c156b456ea9ad40fee4e60b29b7986a6d01
Muhammad Talha <talhaji880@gmail.com>
2024-08-16 23:16:50 +0500

    server setup

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import express, { urlencoded } from 'express'
+import env from "dotenv"
+import cookieParser from 'cookie-parser'
+import cors from "cors"
+
+env.config()
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: "I am comming from backend"
+    })
+})
+//Middlewares
+app.use(express.json())
+app.use(urlencoded({ extended: true }))
+const corsOptions = {
+    origin: "http://localhost:5173",
+    credentials: true,
+}
+
+app.use(cors(corsOptions))
+app.use(cookieParser())
+app.listen(PORT, () => {
+    console.log(`Server Running At ${PORT}`)
+})
